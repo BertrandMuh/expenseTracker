7fbf842b7698ecca8104721dd2c0f53615fea126
BertrandMuh <sangwabertrand14@gmail.com>
2023-10-01 02:07:00 -0500

    add build folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
 import axios from "axios";
+import "./App.css";
 
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { useContext, useEffect } from "react";
